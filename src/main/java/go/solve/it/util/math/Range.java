9e093694f6ae13aee6361ff7209a1261fb870d8d
christophe <christophecreeten@hotmail.com>
2021-12-22 17:08:15 +0100

    Solved 22.

--- src/main/java/go/solve/it/util/math/Range.java
+++ src/main/java/go/solve/it/util/math/Range.java
@@ -7,7 +7,7 @@ import static java.lang.Math.abs;
 import static java.util.Arrays.stream;
 import static java.util.stream.IntStream.iterate;
 
-public sealed class Range<T extends Comparable<T>> permits Range.IntRange {
+public sealed class Range<T extends Comparable<T>> permits Range.IntRange, Range.LongRange {
 
     protected final T from;
     protected final T toInclusive;
@@ -19,6 +19,10 @@ public sealed class Range<T extends Comparable<T>> permits Range.IntRange {
 
     public static IntRange between(final int from, final int toInclusive) {
         return new IntRange(from, toInclusive);
+    }
+
+    public static LongRange between(final long from, final long toInclusive) {
+        return new LongRange(from, toInclusive);
     }
 
     public boolean isIncreasing() {
@@ -33,6 +37,10 @@ public sealed class Range<T extends Comparable<T>> permits Range.IntRange {
         return isIncreasing()
                 ? value.compareTo(from) >= 0 && value.compareTo(toInclusive) <= 0
                 : value.compareTo(toInclusive) >= 0 && value.compareTo(from) <= 0;
+    }
+
+    public boolean overlaps(final Range<T> other) {
+        return contains(other.from) || other.contains(from) || contains(other.toInclusive) || other.contains(toInclusive);
     }
 
     public boolean containsAny(final T... values) {
@@ -66,6 +74,34 @@ public sealed class Range<T extends Comparable<T>> permits Range.IntRange {
                     ? iterate(from(), value -> value <= toInclusive(), value -> value + 1)
                     : iterate(from(), value -> value >= toInclusive(), value -> value - 1))
                     .boxed();
+        }
+    }
+
+    public static final class LongRange extends Range<Long> {
+
+        LongRange(final long from, final long toInclusive) {
+            super(from, toInclusive);
+        }
+
+        public long from() {
+            return from;
+        }
+
+        public long toInclusive() {
+            return toInclusive;
+        }
+
+        public long size() {
+            return abs(toInclusive() - from()) + 1;
+        }
+
+        public boolean isDecreasing() {
+            return from() > toInclusive();
+        }
+
+        @Override
+        public String toString() {
+            return "[" + from() + "," + toInclusive() + "]";
         }
     }
 }
--- src/main/java/go/solve/it/y2021/Day22.java
+++ src/main/java/go/solve/it/y2021/Day22.java
@@ -1,13 +1,16 @@
 package go.solve.it.y2021;
 
+import go.solve.it.util.container.Tuple;
 import go.solve.it.util.input.Input;
 import go.solve.it.util.math.Positions;
 import go.solve.it.util.math.Positions.Position3D;
 import go.solve.it.util.math.Range.IntRange;
-import go.solve.it.util.string.Regex;
+import go.solve.it.util.math.Range.LongRange;
 
 import java.util.HashSet;
+import java.util.Set;
 
+import static go.solve.it.util.string.Regex.findAll;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 import static java.util.Arrays.stream;
@@ -22,7 +25,7 @@ public final class Day22 {
     private static long part1(final String... lines) {
         final var cubes = new HashSet<Position3D>();
         stream(lines).forEach(line -> {
-                    final var numbers = Regex.findAll(line, "(-?\\d+)").stream().mapToInt(Integer::parseInt).toArray();
+                    final var numbers = findAll(line, "(-?\\d+)").stream().mapToInt(Integer::parseInt).toArray();
                     final var xRange = IntRange.between(max(numbers[0], -50), min(numbers[1], 50));
                     final var yRange = IntRange.between(max(numbers[2], -50), min(numbers[3], 50));
                     final var zRange = IntRange.between(max(numbers[4], -50), min(numbers[5], 50));
@@ -39,8 +42,68 @@ public final class Day22 {
         return cubes.size();
     }
 
-
     private static long part2(final String... lines) {
-        return 0;
+        return stream(lines).map(line -> {
+                    final var numbers = findAll(line, "(-?\\d+)").stream().mapToLong(Long::parseLong).toArray();
+                    final var xRange = LongRange.between(numbers[0], numbers[1]);
+                    final var yRange = LongRange.between(numbers[2], numbers[3]);
+                    final var zRange = LongRange.between(numbers[4], numbers[5]);
+                    return Tuple.of(Cube.spanning(xRange, yRange, zRange), line.startsWith("on"));
+                })
+                .reduce((Set<Cube>) new HashSet<Cube>(), (cubes, cube) -> merge(cubes, cube.head(), cube.tail()), (l, r) -> l)
+                .stream()
+                .mapToLong(Cube::size)
+                .sum();
+    }
+
+    public static Set<Cube> merge(final Set<Cube> cubes, final Cube cube, final boolean on) {
+        final var result = new HashSet<Cube>();
+        for (final var c : cubes) {
+            if (c.overlaps(cube)) {
+                result.addAll(split(c, cube));
+            } else {
+                result.add(c);
+            }
+        }
+        if (on) {
+            result.add(cube);
+        }
+        return result;
+    }
+
+    private static Set<Cube> split(final Cube l, final Cube r) {
+        final var result = new HashSet<Cube>();
+        final var xOverlap = LongRange.between(max(l.xRange().from(), r.xRange().from()), min(l.xRange().toInclusive(), r.xRange().toInclusive()));
+        final var zOverlap = LongRange.between(max(l.zRange().from(), r.zRange().from()), min(l.zRange().toInclusive(), r.zRange().toInclusive()));
+        // 'left' slice
+        result.add(Cube.spanning(LongRange.between(l.xRange().from(), r.xRange().from() - 1), l.yRange(), l.zRange()));
+        // 'right' slice
+        result.add(Cube.spanning(LongRange.between(r.xRange().toInclusive() + 1, l.xRange().toInclusive()), l.yRange(), l.zRange()));
+        // 'front' slice
+        result.add(Cube.spanning(xOverlap, l.yRange(), LongRange.between(r.zRange().toInclusive() + 1, l.zRange().toInclusive())));
+        // 'back' slice
+        result.add(Cube.spanning(xOverlap, l.yRange(), LongRange.between(l.zRange().from(), r.zRange().from() - 1)));
+        // 'bottom' slice
+        result.add(Cube.spanning(xOverlap, LongRange.between(l.yRange().from(), r.yRange().from() - 1), zOverlap));
+        // 'top' slice
+        result.add(Cube.spanning(xOverlap, LongRange.between(r.yRange().toInclusive() + 1, l.yRange().toInclusive()), zOverlap));
+        // remove all for which max > min
+        result.removeIf(cube -> cube.xRange().isDecreasing() || cube.yRange().isDecreasing() || cube.zRange().isDecreasing());
+        return result;
+    }
+
+    static final record Cube(LongRange xRange, LongRange yRange, LongRange zRange) {
+
+        static Cube spanning(LongRange xRange, LongRange yRange, LongRange zRange) {
+            return new Cube(xRange, yRange, zRange);
+        }
+
+        private boolean overlaps(final Cube other) {
+            return xRange().overlaps(other.xRange()) && yRange().overlaps(other.yRange()) && zRange().overlaps(other.zRange());
+        }
+
+        public long size() {
+            return xRange().size() * yRange().size() * zRange().size();
+        }
     }
 }
